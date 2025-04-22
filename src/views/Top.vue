c40b32e8c77e0f97b4d2c4a4e137b0b2aaff02b8
test <brucemclarenp4@gmail.com>
2025-04-23 01:23:04 +0900

    トップページ修正

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="flex flex-col items-center justify-center h-screen bg-blue-50">
-    <h1 class="text-4xl font-bold mb-8">日向坂46 推しメン診断(beta)</h1>
+  <div class="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-[#7cc7e8] via-[#add8e6] to-[#7cc7e8]">
+    <h1 class="text-4xl font-bold text-white mb-8">日向坂46 推しメン診断(beta)</h1>
     <router-link to="/question">
       <button
         @click="resetAndStart"
-        class="bg-blue-500 text-white px-6 py-3 rounded-xl hover:bg-blue-600 transition"
+        class="bg-blue-500 text-white px-6 py-3 rounded-xl shadow-lg hover:shadow-xl transition-transform transform hover:scale-105"
       >
         はじめる
       </button>
     </router-link>
-    <div class="mt-3">
+    <div class="mt-3 text-white">
       -更新履歴-
     </div>
-    <div v-for="history in histories">
+    <div v-for="history in histories" :key="history.date" class="text-white">
       {{ history.date }} {{ history.content }}
     </div>
-    1.0.0-beta
+    <div class="mt-4 text-white">
+      1.0.0-beta
+    </div>
   </div>
 </template>
 
