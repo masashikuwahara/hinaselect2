f582f64639253ae752bb38c68ffa380269ad4397
test <brucemclarenp4@gmail.com>
2025-04-25 01:57:06 +0900

    トップページ修正

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-[#7cc7e8] via-[#add8e6] to-[#7cc7e8]">
-    <h1 class="text-center text-4xl font-bold text-white mb-8">HINADX(beta)</h1>
+    <h1 class="text-center text-4xl font-bold text-white mb-2">HINADX(beta)</h1>
+    <p class="text-white mb-8">あなたに合った日向坂46メンバーの推しメンを診断します</p>
     <router-link to="/question">
       <button
         @click="resetAndStart"
@@ -16,7 +17,7 @@
       {{ history.date }} {{ history.content }}
     </div>
     <div class="mt-4 text-white">
-      2.1.1-beta
+      2.1.2-beta
     </div>
   </div>
 </template>
