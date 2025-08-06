d8af4f194991df1bb9c5fdbe8c868663453f2bc1
test <brucemclarenp4@gmail.com>
2025-08-07 00:22:14 +0900

    メンバースコア調整

--- src/data/members.js
+++ src/data/members.js
@@ -1,299 +1,299 @@
 export const members = [
   {
-    name: "金村美玖",
-    categories: {
-      モデル・ビジュアル: 0.5,
-      ダンスが上手い: 0.3,
-      ぶりっ子: 0.3,
-      バラエティ: 0.2,
+    "name": "金村美玖",
+    "categories": {
+      "モデル・ビジュアル": 0.385,
+      "ダンスが上手い": 0.231,
+      "ぶりっ子": 0.231,
+      "バラエティ": 0.154
     },
-    image: "/images/kanemura.jpg", 
-    description: "モデル活動や写真撮影にも積極的。撮影美術に敏感な繊細さと、やわらかな感性を持つ。"
+    "image": "/images/kanemura.jpg",
+    "description": "モデル活動や写真撮影にも積極的。撮影美術に敏感な繊細さと、やわらかな感性を持つ。"
   },
   {
-    name: "河田陽菜",
-    categories: {
-      癒し系: 0.5,
-      天然: 0.3,
-      バラエティ: 0.3,
+    "name": "河田陽菜",
+    "categories": {
+      "癒し系": 0.455,
+      "天然": 0.273,
+      "不思議系": 0.273
     },
-    image: "/images/hina.jpg",
-    description: "愛称「恋愛製造機」。明るくムードメーカータイプで、バラエティ番組での活躍も目立つ。"
+    "image": "/images/hina.jpg",
+    "description": "愛称「恋愛製造機」。明るくムードメーカータイプで、バラエティ番組での活躍も目立つ。"
   },
   {
-    name: "小坂菜緒",
-    categories: {
-      モデル・ビジュアル: 0.5,
-      王道アイドル: 0.3,
-      知的・しっかり者: 0.2,
+    "name": "小坂菜緒",
+    "categories": {
+      "モデル・ビジュアル": 0.5,
+      "王道アイドル": 0.3,
+      "知的・しっかり者": 0.2
     },
-    image: "/images/nao.jpg",
-    description: "ニックネーム「こさかな」。安心感のある存在で、同期からも「いれば大丈夫！」と信頼されている。"
+    "image": "/images/nao.jpg",
+    "description": "ニックネーム「こさかな」。安心感のある存在で、同期からも「いれば大丈夫！」と信頼されている。"
   },
   {
-    name: "富田鈴花",
-    categories: {
-      バラエティ: 0.6,
-      歌唱力が高い: 0.3,
-      スポーツ・アクティブ: 0.3,
+    "name": "富田鈴花",
+    "categories": {
+      "バラエティ": 0.5,
+      "歌唱力が高い": 0.35,
+      "スポーツ・アクティブ": 0.15
     },
-    image: "/images/suzuka.jpg",
-    description: "歌や楽器（ギター、ベース、ピアノ）のマルチな音楽スキルが抜群 。負け癖が強い。"
+    "image": "/images/suzuka.jpg",
+    "description": "歌や楽器（ギター、ベース、ピアノ）のマルチな音楽スキルが抜群 。負け癖が強い。"
   },
   {
-    name: "松田好花",
-    categories: {
-      知的・しっかり者: 0.6,
-      バラエティ: 0.3,
-      歌唱力が高い: 0.1,
+    "name": "松田好花",
+    "categories": {
+      "知的・しっかり者": 0.6,
+      "バラエティ": 0.4
     },
-    image: "/images/konoka.jpg",
-    description: "こちらも多才な才能で様々な場面で活躍するオールラウンダー。ややクレーム気質。"
+    "image": "/images/konoka.jpg",
+    "description": "こちらも多才な才能で様々な場面で活躍するオールラウンダー。ややクレーム気質。"
   },
   {
-    name: "上村ひなの",
-    categories: {
-      天然: 0.5,
-      癒し系: 0.3,
-      王道アイドル: 0.2,
+    "name": "上村ひなの",
+    "categories": {
+      "不思議系": 0.5,
+      "癒し系": 0.3,
+      "天然": 0.2
     },
-    image: "/images/hinano.jpg",
-    description: "天才的な発想と純粋な魅力でファンを惹きつける。"
+    "image": "/images/hinano.jpg",
+    "description": "天才的な発想と純粋な魅力でファンを惹きつける。"
   },
   {
-    name: "髙橋未来虹",
-    categories: {
-      スポーツ・アクティブ: 0.6,
-      知的・しっかり者: 0.3,
-      ダンスが上手い: 0.2,
+    "name": "髙橋未来虹",
+    "categories": {
+      "スポーツ・アクティブ": 0.545,
+      "知的・しっかり者": 0.273,
+      "ダンスが上手い": 0.182
     },
-    image: "/images/mikuni.jpg",
-    description: "高身長を活かしたスタイルと明るい性格で人気上昇中。日向坂46の新キャプテン。"
+    "image": "/images/mikuni.jpg",
+    "description": "高身長を活かしたスタイルと明るい性格で人気上昇中。日向坂46の新キャプテン。"
   },
   {
-    name: "森本茉莉",
-    categories: {
-      バラエティ: 0.7,
-      天然: 0.2,
-      ぶりっ子: 0.1,
+    "name": "森本茉莉",
+    "categories": {
+      "バラエティ": 0.7,
+      "天然": 0.2,
+      "ぶりっ子": 0.1
     },
-    image: "/images/marie.jpg",
-    description: "天真爛漫なキャラクターで番組を盛り上げる。激ヤバお嬢様アイドル。"
+    "image": "/images/marie.jpg",
+    "description": "天真爛漫なキャラクターで番組を盛り上げる。激ヤバお嬢様アイドル。"
   },
   {
-    name: "山口陽世",
-    categories: {
-      スポーツ・アクティブ: 0.6,
-      バラエティ: 0.4,
+    "name": "山口陽世",
+    "categories": {
+      "スポーツ・アクティブ": 0.4,
+      "バラエティ": 0.6
     },
-    image: "/images/haruyo.jpg",
-    description: "アクティブな一面と可愛らしさを兼ね備えるツンデレキャラ。"
+    "image": "/images/haruyo.jpg",
+    "description": "アクティブな一面と可愛らしさを兼ね備えるツンデレキャラ。"
   },
   {
-    name: "石塚瑶季",
-    categories: {
-      バラエティ: 0.6,
-      スポーツ・アクティブ: 0.3,
-      ぶりっ子: 0.1,
+    "name": "石塚瑶季",
+    "categories": {
+      "バラエティ": 0.6,
+      "スポーツ・アクティブ": 0.2,
+      "ぶりっ子": 0.2
     },
-    image: "/images/tamaki.jpg",
-    description: "いつでも元気!柔らかな笑顔で周囲を和ませる存在。"
+    "image": "/images/tamaki.jpg",
+    "description": "いつでも元気!柔らかな笑顔で周囲を和ませる存在。"
   },
   {
-    name: "小西夏菜実",
-    categories: {
-      モデル・ビジュアル: 0.6,
-      バラエティ: 0.4,
-      ダンスが上手い: 0.1,
+    "name": "小西夏菜実",
+    "categories": {
+      "モデル・ビジュアル": 0.545,
+      "バラエティ": 0.364,
+      "ダンスが上手い": 0.091
     },
-    image: "/images/nanami.jpg",
-    description: "関西弁を話す美容番長。キャッチフレーズや毒舌もあり、愛され天然キャラ "
+    "image": "/images/nanami.jpg",
+    "description": "関西弁を話す美容番長。キャッチフレーズや毒舌もあり、愛され天然キャラ "
   },
   {
-    name: "清水理央",
-    categories: {
-      スポーツ・アクティブ: 0.7,
-      王道アイドル: 0.3,
+    "name": "清水理央",
+    "categories": {
+      "スポーツ・アクティブ": 0.7,
+      "王道アイドル": 0.3
     },
-    image: "/images/rio.jpg",
-    description: "甲子園出場のチア経験を持ち、センター経験もある運動神経抜群なストイックガール。"
+    "image": "/images/rio.jpg",
+    "description": "甲子園出場のチア経験を持ち、センター経験もある運動神経抜群なストイックガール。"
   },
   {
-    name: "正源司陽子",
-    categories: {
-      王道アイドル: 0.6,
-      知的・しっかり者: 0.3,
-      歌唱力が高い: 0.3,
+    "name": "正源司陽子",
+    "categories": {
+      "王道アイドル": 0.5,
+      "知的・しっかり者": 0.25,
+      "歌唱力が高い": 0.25
     },
-    image: "/images/youko.jpg",
-    description: "楽器演奏（ギター・バイオリン・フルート）に長け、4期生のエース的存在。"
+    "image": "/images/youko.jpg",
+    "description": "楽器演奏（ギター・バイオリン・フルート）に長け、4期生のエース的存在。"
   },
   {
-    name: "竹内希来里",
-    categories: {
-      癒し系: 0.4,
-      モデル・ビジュアル: 0.3,
-      王道アイドル: 0.3,
+    "name": "竹内希来里",
+    "categories": {
+      "癒し系": 0.4,
+      "不思議系": 0.3,
+      "王道アイドル": 0.3
     },
-    image: "/images/kirari.jpg",
-    description: "地元テレビ番組に出演し活動的。強運を持つと評され、社交的で明るい性格。"
+    "image": "/images/kirari.jpg",
+    "description": "地元テレビ番組に出演し活動的。強運を持つと評され、社交的で明るい性格。"
   },
   {
-    name: "平尾帆夏",
-    categories: {
-      知的・しっかり者: 0.6,
-      癒し系: 0.3,
-      バラエティ: 0.1,
+    "name": "平尾帆夏",
+    "categories": {
+      "知的・しっかり者": 0.6,
+      "不思議系": 0.3,
+      "バラエティ": 0.1
     },
-    image: "/images/hiraho.jpg",
-    description: "幼少期からピアノを嗜み、語彙豊富な毒舌キャラ。才女 × バラエティ。"
+    "image": "/images/hiraho.jpg",
+    "description": "幼少期からピアノを嗜み、語彙豊富な毒舌キャラ。才女 × バラエティ。"
   },
   {
-    name: "平岡海月",
-    categories: {
-      歌唱力が高い: 0.5,
-      バラエティ: 0.3,
-      スポーツ・アクティブ: 0.2,
+    "name": "平岡海月",
+    "categories": {
+      "歌唱力が高い": 0.6,
+      "バラエティ": 0.2,
+      "ダンスが上手い": 0.2
     },
-    image: "/images/mitsuki.jpg",
-    description: "4期生最年長。お笑い好きでMCが得意、まとめ役として安定した存在。"
+    "image": "/images/mitsuki.jpg",
+    "description": "4期生最年長。お笑い好きでMCが得意、まとめ役として安定した存在。"
   },
   {
-    name: "藤嶌果歩",
-    categories: {
-      歌唱力が高い: 0.5,
-      癒し系: 0.3,
-      王道アイドル: 0.2,
+    "name": "藤嶌果歩",
+    "categories": {
+      "歌唱力が高い": 0.5,
+      "癒し系": 0.3,
+      "王道アイドル": 0.2
     },
-    image: "/images/kaho.jpg",
-    description: "書道と歌が得意。北海道出身、物静かで芯のある性格。"
+    "image": "/images/kaho.jpg",
+    "description": "書道と歌が得意。北海道出身、物静かで芯のある性格。"
   },
   {
-    name: "宮地すみれ",
-    categories: {
-      ぶりっ子: 0.7,
-      ダンスが上手い: 0.2,
-      知的・しっかり者: 0.1,
+    "name": "宮地すみれ",
+    "categories": {
+      "ぶりっ子": 0.7,
+      "ダンスが上手い": 0.2,
+      "知的・しっかり者": 0.1
     },
-    image: "/images/sumire.jpg",
-    description: "愛されぶりっ子キャラ。気配り上手で芯が強く引き締め役。"
+    "image": "/images/sumire.jpg",
+    "description": "愛されぶりっ子キャラ。気配り上手で芯が強く引き締め役。"
   },
   {
-    name: "山下葉留花",
-    categories: {
-      天然: 0.7,
-      バラエティ: 0.2,
-      癒し系: 0.1,
+    "name": "山下葉留花",
+    "categories": {
+      "天然": 0.5,
+      "不思議系": 0.3,
+      "バラエティ": 0.2,
     },
-    image: "/images/haruka.jpg",
-    description: "元OLで社会人経験あり。優しい性格、“聖母”タイプ。"
+    "image": "/images/haruka.jpg",
+    "description": "元OLで社会人経験あり。優しい性格、“聖母”タイプ。"
   },
   {
-    name: "渡辺莉奈",
-    categories: {
-      王道アイドル: 0.4,
-      知的・しっかり者: 0.3,
-      バラエティ: 0.3,
+    "name": "渡辺莉奈",
+    "categories": {
+      "王道アイドル": 0.4,
+      "知的・しっかり者": 0.3,
+      "バラエティ": 0.3
     },
-    image: "/images/rina.jpg",
-    description: "最年少で落ち着いた雰囲気。芸達者でCM出演経験もあり。"
+    "image": "/images/rina.jpg",
+    "description": "最年少で落ち着いた雰囲気。芸達者でCM出演経験もあり。"
   },
   {
-    name: "大田美月",
-    categories: {
-      ダンスが上手い:0.5,
-      天然: 0.3,
-      バラエティ: 0.2,
+    "name": "大田美月",
+    "categories": {
+      "ダンスが上手い": 0.5,
+      "不思議系": 0.4,
+      "バラエティ": 0.1
     },
-    image: "/images/oota_mizuki.jpg",
-    description: "ピンクが大好きの不思議少女。",
+    "image": "/images/mizuki.jpg",
+    "description": "ピンクが大好きの不思議少女。"
   },
   {
-    name: "大野愛実",
-    categories: {
-      王道アイドル: 0.5,
-      モデル・ビジュアル: 0.3,
-      癒し系: 0.2,
+    "name": "大野愛実",
+    "categories": {
+      "王道アイドル": 0.5,
+      "ぶりっ子": 0.3,
+      "癒し系": 0.2
     },
-    image: "/images/oono_manami.jpg",
-    description: "新世代の王道アイドル。",
+    "image": "/images/manami.jpg",
+    "description": "新世代の王道アイドル。"
   },
   {
-    name: "片山紗希",
-    categories: {
-      モデル・ビジュアル: 0.5,
-      スポーツ・アクティブ: 0.3,
-      王道アイドル: 0.2,
+    "name": "片山紗希",
+    "categories": {
+      "モデル・ビジュアル": 0.5,
+      "不思議系": 0.4,
+      "王道アイドル": 0.1
     },
-    image: "/images/katayama_saki.jpg",
-    description: "チアダンスが得意なウサギっ子。",
+    "image": "/images/saki.jpg",
+    "description": "チアダンスが得意なウサギっ子。"
   },
   {
-    name: "蔵盛妃那乃",
-    categories: {
-      癒し系: 0.4,
-      ダンスが上手い: 0.3,
-      王道アイドル: 0.3,
+    "name": "蔵盛妃那乃",
+    "categories": {
+      "癒し系": 0.4,
+      "ダンスが上手い": 0.3,
+      "王道アイドル": 0.3
     },
-    image: "/images/kuramori_hinano.jpg",
-    description: "日本舞踊が得意なはんなり和風娘。",
+    "image": "/images/hinanon.jpg",
+    "description": "日本舞踊が得意なはんなり和風娘。"
   },
   {
-    name: "坂井新奈",
-    categories: {
-      王道アイドル: 0.4,
-      天然: 0.2,
-      癒し系: 0.2,
+    "name": "坂井新奈",
+    "categories": {
+      "王道アイドル": 0.5,
+      "天然": 0.25,
+      "不思議系": 0.25
     },
-    image: "/images/sakai_niina.jpg",
-    description: "ちょっとおバカな帰国子女。",
+    "image": "/images/niina.jpg",
+    "description": "ちょっと不思議な帰国子女。"
   },
   {
-    name: "佐藤優羽",
-    categories: {
-      知的・しっかり者: 0.6,
-      スポーツ・アクティブ: 0.2,
-      ぶりっ子: 0.2,
+    "name": "佐藤優羽",
+    "categories": {
+      "知的・しっかり者": 0.6,
+      "スポーツ・アクティブ": 0.2,
+      "ぶりっ子": 0.2
     },
-    image: "/images/satou_yuu.jpg",
-    description: "萌え声を持つ薙刀麻雀少女。",
+    "image": "/images/yuu.jpg",
+    "description": "萌え声を持つ薙刀麻雀少女。"
   },
   {
-    name: "下田衣珠季",
-    categories: {
-      天然: 0.6,
-      癒し系: 0.4,
+    "name": "下田衣珠季",
+    "categories": {
+      "天然": 0.6,
+      "癒し系": 0.3,
+      "バラエティ": 0.1
     },
-    image: "/images/shimoda_izuki.jpg",
-    description: "おバカなニューヒロイン。",
+    "image": "/images/izuki.jpg",
+    "description": "おバカなニューヒロイン。"
   },
   {
-    name: "高井俐香",
-    categories: {
-      知的・しっかり者: 0.5,
-      王道アイドル: 0.3,
-      癒し系: 0.2,
+    "name": "高井俐香",
+    "categories": {
+      "知的・しっかり者": 0.5,
+      "王道アイドル": 0.3,
+      "不思議系": 0.2
     },
-    image: "/images/takai_rika.jpg",
-    description: "虎党で乗馬が得意なお嬢様アイドル。",
+    "image": "/images/rika.png",
+    "description": "虎党で乗馬が得意なお嬢様アイドル。"
   },
   {
-    name: "鶴崎仁香",
-    categories: {
-      知的・しっかり者: 0.6,
-      癒し系: 0.4,
+    "name": "鶴崎仁香",
+    "categories": {
+      "知的・しっかり者": 0.6,
+      "癒し系": 0.4
     },
-    image: "/images/tsurusaki_niko.jpg",
-    description: "知的で冷静なしっかり者",
+    "image": "/images/niko.jpg",
+    "description": "知的で冷静なしっかり者"
   },
   {
-    name: "松尾桜",
-    categories: {
-      モデル・ビジュアル: 0.5,
-      歌唱力が高い: 0.4,
-      王道アイドル: 0.1,
+    "name": "松尾桜",
+    "categories": {
+      "モデル・ビジュアル": 0.5,
+      "歌唱力が高い": 0.4,
+      "王道アイドル": 0.1
     },
-    image: "/images/matsuo_sakura.jpg",
-    description: "料理が得意なマイペース",
-  },
+    "image": "/images/sakura.jpg",
+    "description": "料理が得意なマイペース"
+  }
 ];
